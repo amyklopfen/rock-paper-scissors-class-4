671c64c679bcbcd117afd461cbac74a14bc92f33
amyklopfen <50849294+amyklopfen@users.noreply.github.com>
2019-06-05 20:50:36 -0400

    incorporate test code

--- game.py
+++ game.py
@@ -1,39 +1,48 @@
 import random
 
-#capture inputs 
-print("welcome to rock, paper, scissors, shoot.")
-user_input = input("please choose one of the following options: rock, paper, or scissors ")
-x = user_input
+def my_message():
+    return "Hello"
 
-game_list = ["rock","paper","scissors"]
+x = my_message()
+print(x)
 
-computer = random.choice(game_list)
+#only if this code is executed from the command line
+if __name__ == "__main__":
+    
+    #capture inputs 
+    print("welcome to rock, paper, scissors, shoot.")
+    user_input = input("please choose one of the following options: rock, paper, or scissors ")
+    x = user_input
 
-#generate computer inputs
-print("you chose", x)
-print("computer chose", computer)
+    game_list = ["rock","paper","scissors"]
 
-#validate inputs
-if x not in game_list:
-    print("error, invalid entry. enter rock, paper, or scissors")
-    exit()   
+    computer = random.choice(game_list)
 
-#determine the winner
-if x == computer: #== compares user choice to computer choice; not assigning value to a variable
-    print("it's a tie")
-elif x == "rock" and computer == "paper":
-    print("you lose!")
-elif x == "rock" and computer == "scissors":
-    print("you win!")
-elif x == "paper" and computer == "scissors":
-    print("you lose!")
-elif x == "paper" and computer == "rock":
-    print("you win!")
-elif x == "scissors" and computer == "rock":
-    print("you win!")
-elif x == "scissors" and computer == "paper":
-    print("you win!")#display the final outputs/outcomes
+    #generate computer inputs
+    print("you chose", x)
+    print("computer chose", computer)
 
-print("thanks for playing!")
+    #validate inputs
+    if x not in game_list:
+        print("error, invalid entry. enter rock, paper, or scissors")
+        exit()   
+
+    #determine the winner
+    if x == computer: #== compares user choice to computer choice; not assigning value to a variable
+        print("it's a tie")
+    elif x == "rock" and computer == "paper":
+        print("you lose!")
+    elif x == "rock" and computer == "scissors":
+        print("you win!")
+    elif x == "paper" and computer == "scissors":
+        print("you lose!")
+    elif x == "paper" and computer == "rock":
+        print("you win!")
+    elif x == "scissors" and computer == "rock":
+        print("you win!")
+    elif x == "scissors" and computer == "paper":
+        print("you win!")#display the final outputs/outcomes
+
+    print("thanks for playing!")
 
 
--- game_test.py
+++ game_test.py
@@ -1,2 +1,9 @@
+from game import my_message
+
 def test_example():
     assert 2 == 2
+
+def test_my_message():
+    x = my_message()
+    assert x == "Hello"
+
